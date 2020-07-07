9b4e4e0dcad6c48e4c2b6d259516c290f9f51869
Youssef Hany <youssefhany30@yahoo.com>
2020-07-07 14:28:59 +0200

    Second Commit

--- ml.py
+++ ml.py
@@ -1,5 +1,6 @@
 import re
-from base_functions import *
+import os
+import pandas as pd
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
@@ -18,6 +19,15 @@ maxlen = 100
 #        doc_term_matrix = vectorizer.fit_transform(message_list)
 #        return DataFrame(doc_term_matrix.toarray())
 
+def file_to_list(file_name):
+    if not os.path.isfile(file_name):
+        print("[-BF] Could not find file path exiting...")
+	
+    headers = ["payload", "text","state"]
+    df = pd.read_csv(file_name)
+    return df
+
+
 file_data = file_to_list("learn_data.csv")
 html_data = file_data["html"].values
 labels = file_data["state"].values
@@ -27,16 +37,17 @@ tokenizer = Tokenizer(num_words=5000)
 tokenizer.fit_on_texts(train_data)
 X_train = tokenizer.texts_to_sequences(train_data)
 X_test = tokenizer.texts_to_sequences(test_data)
+tokenizer.word_index["<PAD>"] = 0
 vocab_size = len(tokenizer.word_index) + 1
-X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
-X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+X_train = pad_sequences(X_train, value=tokenizer.word_index["<PAD>"], padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, value=tokenizer.word_index["<PAD>"], padding='post', maxlen=maxlen)
 embedding_dim = 50
 model = Sequential()
 model.add(layers.Embedding(input_dim=vocab_size, 
                            output_dim=embedding_dim, 
                            input_length=maxlen))
-model.add(layers.GlobalMaxPool1D())
-model.add(layers.Dense(10, activation='relu'))
+model.add(layers.GlobalAveragePooling1D())
+model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
